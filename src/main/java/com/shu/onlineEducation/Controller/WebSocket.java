04f67168e1e0e954adf665cc743cdddb8e4adea9
Hyq0719 <2418011730@qq.com>
2021-01-04 13:25:09 +0800

    debug WebSocket

--- src/main/java/com/shu/onlineEducation/Controller/WebSocket.java
+++ src/main/java/com/shu/onlineEducation/Controller/WebSocket.java
@@ -1,8 +1,9 @@
 package com.shu.onlineEducation.Controller;
 
-
 import com.shu.onlineEducation.Dao.StudentJpaRepository;
+import com.shu.onlineEducation.Dao.TeacherJpaRepository;
 import com.shu.onlineEducation.Entity.Student;
+import com.shu.onlineEducation.Entity.Teacher;
 import com.shu.onlineEducation.utils.Result.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +23,7 @@ import java.util.*;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 @RestController
-@ServerEndpoint(value = "/websocket/{sid}/{StudentId}")   //房间号、类型（1，直播聊天）、用户Id、
+@ServerEndpoint(value = "/websocket/{sid}/{type}/{UserId}")   //房间号、类型（1，直播聊天）、用户Id、
 @Component
 public class WebSocket {
 	private static WebSocket webSocket;
@@ -35,12 +36,16 @@ public class WebSocket {
 	private Session session;
 	//客户端唯一标识sid(直播ID)
 	private String sid = "";
+	//用户类型
+	private Integer type = 0;
 	//学生ID
-	private Integer studentId = 0;
+	private Integer userId = 0;
 	//用户昵称
 	private String nickName = "";
 	@Autowired
 	private StudentJpaRepository studentJpaRepository;
+	@Autowired
+	private TeacherJpaRepository teacherJpaRepository;
 	
 	public void sendMessage(String message) throws IOException {
 		this.session.getBasicRemote().sendText(message);
@@ -50,22 +55,31 @@ public class WebSocket {
 	public void init() {
 		webSocket = this;
 		webSocket.studentJpaRepository = this.studentJpaRepository;
+		webSocket.teacherJpaRepository = this.teacherJpaRepository;
 	}
 	
 	//连接建立成功调用的方法
 	@OnOpen
-	public void onOpen(Session session, @PathParam("sid") String sid, @PathParam("studentId") Integer studentId) {
-		moreWindow(sid, studentId);
+	public void onOpen(Session session, @PathParam("sid") String sid, @PathParam("type") Integer type, @PathParam("userId") Integer userId) {
+		moreWindow(sid, type, userId);
 		//在线数加1
 		addOnlineCount();
 		this.session = session;
+		this.sid = sid;
+		this.type = type;
+		if (type.equals(0)) {
+			this.userId = userId;
+			Student student = WebSocket.webSocket.studentJpaRepository.findStudentByUserId(userId);
+			this.nickName = student.getNickName();
+			logger.info("学生ID:" + userId + "学生昵称:" + nickName + "新连接：sid=" + sid + " 当前在线人数" + getOnlineCount());
+		} else if (type.equals(1)) {
+			this.userId = userId;
+			Teacher teacher = WebSocket.webSocket.teacherJpaRepository.findTeacherByUserId(userId);
+			this.nickName = teacher.getName();
+			logger.info("老师ID:" + userId + "老师名字:" + nickName + "新连接：sid=" + sid + " 当前在线人数" + getOnlineCount());
+		}
 		//加入set中
 		webSocketSet.add(this);
-		this.sid = sid;
-		this.studentId = studentId;
-		Student student = WebSocket.webSocket.studentJpaRepository.findStudentByUserId(studentId);
-		this.nickName = student.getNickName();
-		logger.info("用户ID:" + studentId + "用户昵称:" + nickName + "新连接：sid=" + sid + " 当前在线人数" + getOnlineCount());
 		try {
 			sendMessage("连接成功");
 		} catch (IOException e) {
@@ -90,36 +104,36 @@ public class WebSocket {
 	}
 	
 	// 同一用户打开多个窗口问题
-	public void moreWindow(String sid, Integer studentId) {
+	public void moreWindow(String sid, Integer userId, Integer type) {
 		if (webSocketSet.isEmpty()) {
 			return;
 		}
 		for (WebSocket item : webSocketSet) {
-			if (item.sid.equals(sid) && item.studentId.equals(studentId)) {
+			if (item.sid.equals(sid) && item.userId.equals(userId) && item.type.equals(type)) {
 				//已经有相同的了
 				webSocketSet.remove(item);
 				subOnlineCount();
 			}
 		}
 	}
-	
-	//发送消息给指定用户
-	public static void sendMessage(String message, @PathParam("sid") String sid) {
-		logger.info("发送消息：sid=" + sid + " message：" + message);
-		for (WebSocket item : webSocketSet) {
-			try {
-				if (sid == null) {
-					item.sendMessage(message);
-					logger.info("+++++++++++++++" + message);
-				} else if (item.sid.equals(sid)) {
-					logger.info("开始发送消息：sid=" + sid);
-					item.sendMessage(message);
-				}
-			} catch (IOException e) {
-				logger.error("发送消息失败 sid:" + sid, e);
-			}
-		}
-	}
+
+//	//发送消息给指定用户
+//	public static void sendMessage(String message, @PathParam("sid") String sid) {
+//		logger.info("发送消息：sid=" + sid + " message：" + message);
+//		for (WebSocket item : webSocketSet) {
+//			try {
+//				if (sid == null) {
+//					item.sendMessage(message);
+//					logger.info("+++++++++++++++" + message);
+//				} else if (item.sid.equals(sid)) {
+//					logger.info("开始发送消息：sid=" + sid);
+//					item.sendMessage(message);
+//				}
+//			} catch (IOException e) {
+//				logger.error("发送消息失败 sid:" + sid, e);
+//			}
+//		}
+//	}
 	
 	@OnClose
 	public void onClose() {
@@ -153,22 +167,29 @@ public class WebSocket {
 	}
 	
 	//人数列表
-	@PostMapping(value = "api/WebSocket/studentList")
+	@PostMapping(value = "api/WebSocket/userList")
 	@ResponseBody
-	public Result studentList(@RequestParam(value = "sid") String sid) {
-		Map map = new HashMap<>();
+	public Result userList(@RequestParam(value = "sid") String sid) {
+		Map<String, Object> map = new HashMap<>();
 		List<Student> studentList = new ArrayList<>();
+		Teacher teacher = new Teacher();
 		Integer count = 0;
 		for (WebSocket item : webSocketSet) {
-			if (item.sid != null && item.sid.equals(sid)) {
-				Student student = new Student();
-				student.setNickName(item.nickName);
-				student.setUserId(item.studentId);
-				studentList.add(student);
+			if (sid.equals(item.sid)) {
+				if (item.type.equals(0)) {
+					Student student = new Student();
+					student.setNickName(item.nickName);
+					student.setUserId(item.userId);
+					studentList.add(student);
+				} else if (item.type.equals(1)) {
+					teacher.setName(item.nickName);
+					teacher.setUserId(item.userId);
+				}
 				count++;
 			}
 		}
 		map.put("studentList", studentList);
+		map.put("teacher", teacher);
 		map.put("count", count);
 		return Result.success(map);
 	}
