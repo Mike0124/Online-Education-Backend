bf6d2aa24674b3a41d98b85456ea23cf9cea1924
Hyq0719 <2418011730@qq.com>
2021-01-06 15:08:52 +0800

    debug SwaggerConfig

--- src/main/java/com/shu/onlineEducation/config/SwaggerConfig.java
+++ src/main/java/com/shu/onlineEducation/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.shu.onlineEducation.Controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.shu.onlineEducation.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
